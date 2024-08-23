cb2ca467b603d87f0cd030d3fe739e49a80cb4a8
alphanetworkss <160410546+alphanetworkss@users.noreply.github.com>
2024-08-23 14:53:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -149,7 +149,7 @@ async def upload(bot: Client, m: Message):
 
             elif '/master.mpd' in url:
              id =  url.split("/")[-2]
-             url =  "https://madxabhi-api-dl-00c151fc6ebd.herokuapp.com/pw/" + id + "/master.m3u8&token=3a9888e257f3ad46238785713861d074057ed2c394eae542dbbd16557351333d"
+             url =  "https://d26g5bnklkwsh4.cloudfront.net/" + id + "/master.m3u8"
 
             name1 = links[i][0].replace("\t", "").replace(":", "").replace("/", "").replace("+", "").replace("#", "").replace("|", "").replace("@", "").replace("*", "").replace(".", "").replace("https", "").replace("http", "").strip()
             name = f'{str(count).zfill(3)}) {name1[:60]}'
